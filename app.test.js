7c9965e866550b7ddd26916be88c8a331f5e77d0
Jorge Pires <jorgeguerrapires@yahoo.com.br>
2021-06-19 15:33:12 -0300

    19 06 21

--- app.test.js
+++ app.test.js
@@ -1,6 +1,8 @@
 require("./app");
 const mongoose = require('mongoose');
 const Kitten = mongoose.model("Kitten");
+const mockingoose = require('mockingoose');
+
 
 
 
@@ -28,3 +30,40 @@ describe('Testando os métodos associados ao nosso modelo de gatos fofinhos', ()
         expect(silence.greeting()).toBe(`Miau, meu nome é ${name}`);
     })
 })
+
+
+describe('Testando o método save, built-in do Mongoose', () => {
+
+    /**test 1: estamos testando se o método greeting ainda funciona depois de ser retornado  
+    */
+    it('Estamos testando se o método greeting está funcionando depois de salvar no Mongo', () => {
+
+        const name = 'Silence';
+        const doc = { name: name };
+        const silence = new Kitten(doc);
+
+        mockingoose(Kitten).toReturn(doc, 'save');
+
+        silence.save((err, fluffy) => {
+            if (err) return console.error(err);
+            expect(fluffy.greeting()).toBe(`Miau, meu nome é ${name}`);
+        })
+    })
+
+    //app.test.js
+    /**teste 2: será se o id devolvido respeita os padrões do MongoDB?*/
+
+    it('Estamos testando se o _id bate depois de salvar', () => {
+        const name = 'Silence';
+        const doc = { name: name };
+        const silence = new Kitten({ name: name });
+
+        mockingoose(Kitten).toReturn(doc, 'save');
+
+        silence.save(function (err, fluffy) {
+            if (err) return console.error(err);
+            expect(objectid.isValid(fluffy._id)).toBeTruthy();
+        })
+    })
+})
+
